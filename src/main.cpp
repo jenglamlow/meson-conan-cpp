040e9f048b0ba3339a6451ec912fd3c20d4c977b
Low Jeng Lam <jenglamlow@gmail.com>
2018-08-02 17:29:34 +0800

    Fix using C libraries in CPP file

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
-#include <libavcodec/avcodec.h>
+extern "C"
+{
 #include <libavformat/avformat.h>
+#include <libavcodec/avcodec.h>
+#include <libavutil/avutil.h>
 #include <libswscale/swscale.h>
+}
 
 #include <iostream>
 
 int main(int argc, char** argv){
     std::cout<< "Hello World!\n";
+
+    av_log_set_flags(AV_LOG_SKIP_REPEATED);
 }
